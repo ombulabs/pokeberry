3beea445db77cacbfe394580ec0ae5b455e2d919
Henrique Borges <henriqueborgeshbr@gmail.com>
2022-06-13 14:08:54 -0300

    chore: remove `instrumented` directory

--- instrumented/components/TestComponent/TestComponent.tsx
+++
@@ -1,9 +0,0 @@
-export interface TestComponentProps {
-  foo: string
-}
-
-const TestComponent: React.FC<TestComponentProps> = (props) => {
-  return <div>Hello, world!</div>;
-};
-
-export default TestComponent;
