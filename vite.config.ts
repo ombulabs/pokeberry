ef521289963c785e20b76ff96d39bf31f11a4242
Kostiantyn Popovych <kostiantyn.popovych.exp@gmail.com>
2022-08-22 11:21:12 -0700

    Revert tests coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+//@ts-ignore
+import istanbul from 'vite-plugin-istanbul';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    istanbul({
+      include: 'packages/*',
+      exclude: ['node_modules'],
+      extension: ['.tsx', '.ts'],
+      cypress: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@utils': path.resolve(__dirname, './utils/index'),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
 });
